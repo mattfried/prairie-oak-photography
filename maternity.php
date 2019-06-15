0c1c7e5d772957dbf0e6e7c0c994a103b0517764
Matt Fried <mattfrieder@gmail.com>
2019-06-15 13:14:07 -0500

    added maternity img's and updated maternity.php img references

--- maternity.php
+++ maternity.php
@@ -17,14 +17,32 @@
 
 
   <!-- ENGAGEMENT IMAGES -->
-  <img class="portfolio-img" src="img/sample1.jpeg" alt="" />
-  <img class="portfolio-img" src="img/sample2.jpeg" alt="" />
-  <img class="portfolio-img" src="img/sample3.jpeg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-01.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-02.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-03.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-04.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-05.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-06.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-07.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-08.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-09.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-10.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-11.jpg" alt="" />
+  <img class="portfolio-img" src="img/maternity/mat-12.jpg" alt="" />
   <!-- /ENGAGEMENT IMAGES -->
 
   <!--  PREVIEW ROW (need to match # of 'rect' <span> elements to the # of IMAGES)  -->
   <div class="preview-row">
     <span class="preview target"></span>
+    <span class="preview"></span>
+    <span class="preview"></span>
+    <span class="preview"></span>
+    <span class="preview"></span>
+    <span class="preview"></span>
+    <span class="preview"></span>
+    <span class="preview"></span>
+    <span class="preview"></span>
+    <span class="preview"></span>
     <span class="preview"></span>
     <span class="preview"></span>
   </div>
