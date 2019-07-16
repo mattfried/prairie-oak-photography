902a8a1bd8c3d98e0c327963c0cdf6e2a824d51e
Matt Fried <mattfrieder@gmail.com>
2019-07-16 17:39:21 -0500

    provided family imgs in family.php

--- family.php
+++ family.php
@@ -18,11 +18,23 @@
       <?php include('inc/pageTitle.php');?>
 
 
-      <!-- ENGAGEMENT IMAGES -->
-      <img class="portfolio-img" src="img/sample1.jpeg" alt="" />
-      <img class="portfolio-img" src="img/sample2.jpeg" alt="" />
-      <img class="portfolio-img" src="img/sample3.jpeg" alt="" />
-      <!-- /ENGAGEMENT IMAGES -->
+      <!-- FAMILY IMAGES -->
+      <img class="portfolio-img" src="img/family/fam-01.jpg" alt="family sitting in snowy field" />
+      <img class="portfolio-img" src="img/family/fam-02.jpg" alt="husband and wife kissing in Christmas tree field with children held and on shoulders" />
+      <img class="portfolio-img" src="img/family/fam-03.jpg" alt="husband and wife swinging children in the snow" />
+      <img class="portfolio-img" src="img/family/fam-04.jpg" alt="family sitting on brick walkway with stone wall behind" />
+      <img class="portfolio-img" src="img/family/fam-05.jpg" alt="family standing on loading dock infront of brick wall" />
+      <img class="portfolio-img" src="img/family/fam-06.jpg" alt="family snuggling under blanket in a cold winter field" />
+      <img class="portfolio-img" src="img/family/fam-07.jpg" alt="husband and wife holding children in snowy field" />
+      <img class="portfolio-img" src="img/family/fam-08.jpg" alt="young boy walking assisted over fall leaves with the help of mom and dad" />
+      <img class="portfolio-img" src="img/family/fam-09.jpg" alt="husband wife and dog posing in a park" />
+      <img class="portfolio-img" src="img/family/fam-10.jpg" alt="family gathered in shallow water with mom and dad kissing" />
+      <img class="portfolio-img" src="img/family/fam-11.jpg" alt="family sitting on stone steps cuddling eachother" />
+      <img class="portfolio-img" src="img/family/fam-12.jpg" alt="young brothers posing infront of old stone wall" />
+      <img class="portfolio-img" src="img/family/fam-13.jpg" alt="family sitting by a fire in distant campground" />
+      <img class="portfolio-img" src="img/family/fam-14.jpg" alt="young boy holding hands with grandma and grandpa" />
+      <img class="portfolio-img" src="img/family/fam-15.jpg" alt="multi generational family photo in backyard" />
+      <!-- /FAMILY IMAGES -->
 
       <!--  PREVIEW ROW (need to match # of 'rect' <span> elements to the # of IMAGES)  -->
       <div class="preview-row">
