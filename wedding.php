5118345e2607fed56693604958b6435040f9826a
Matt Fried <mattfrieder@gmail.com>
2019-07-16 17:36:23 -0500

    provided wedding photos in wedding.php

--- wedding.php
+++ wedding.php
@@ -4,7 +4,7 @@
       $pageTitle = 'Wedding';
       $arrows = true;
       $portfolio = true;
-      
+
       // HEADER
       include('inc/header.php');
     ?>
@@ -18,11 +18,23 @@
       <?php include('inc/pageTitle.php');?>
 
 
-      <!-- ENGAGEMENT IMAGES -->
-      <img class="portfolio-img" src="img/sample1.jpeg" alt="" />
-      <img class="portfolio-img" src="img/sample2.jpeg" alt="" />
-      <img class="portfolio-img" src="img/sample3.jpeg" alt="" />
-      <!-- /ENGAGEMENT IMAGES -->
+      <!-- WEDDING IMAGES -->
+      <img class="portfolio-img" src="img/wedding/wed-01.jpg" alt="newlyweds holding hands infront of wedding doors" />
+      <img class="portfolio-img" src="img/wedding/wed-02.jpg" alt="couple holding eachother in the middle of long outdoor wood staircase" />
+      <img class="portfolio-img" src="img/wedding/wed-03.jpg" alt="couple leaning in for kiss along pathway in forest" />
+      <img class="portfolio-img" src="img/wedding/wed-04.jpg" alt="newlyweds holding eachother close with eyes closed" />
+      <img class="portfolio-img" src="img/wedding/wed-05.jpg" alt="bride and groom wedding party" />
+      <img class="portfolio-img" src="img/wedding/wed-06.jpg" alt="couple holding each other in woods leaning against old wood shed" />
+      <img class="portfolio-img" src="img/wedding/wed-07.jpg" alt="newlyweds holding each other along ocean" />
+      <img class="portfolio-img" src="img/wedding/wed-08.jpg" alt="newlyweds holding each other on ocean shore beach" />
+      <img class="portfolio-img" src="img/wedding/wed-09.jpg" alt="bridal party posing on rocks along a creek" />
+      <img class="portfolio-img" src="img/wedding/wed-10.jpg" alt="bride wrapping arms around her groom from behind" />
+      <img class="portfolio-img" src="img/wedding/wed-11.jpg" alt="newlyweds kissing in wooded backyard" />
+      <img class="portfolio-img" src="img/wedding/wed-12.jpg" alt="newlyweds standing close touching foreheads" />
+      <img class="portfolio-img" src="img/wedding/wed-13.jpg" alt="newlyweds standing close on ocean shore beach" />
+      <img class="portfolio-img" src="img/wedding/wed-14.jpg" alt="bride wrapping arms around her broom in large culvert" />
+      <img class="portfolio-img" src="img/wedding/wed-15.jpg" alt="newlyweds holding eachother in wheat field" />
+      <!-- /WEDDING IMAGES -->
 
       <!--  PREVIEW ROW (need to match # of 'rect' <span> elements to the # of IMAGES)  -->
       <div class="preview-row">
